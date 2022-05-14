b5dd5d1a10d6f9b201daf90957e53237d01a5fff
Alex B <alexander.benteby@gmail.com>
2022-05-14 16:36:30 +0200

    fixed DA offerCalc

--- src/main/java/se/oru/coordination/coordination_oru/MAS/DrawAgent.java
+++ src/main/java/se/oru/coordination/coordination_oru/MAS/DrawAgent.java
@@ -155,17 +155,18 @@ public class DrawAgent extends BidderAgent{
         int oreEval =  (int) (Math.abs(t.ore) > this.TAcapacity-0.1 ? 1000*this.ORE_WEIGHT : this.linearDecreasingComparingFunc(Math.abs(t.ore), this.TAcapacity, this.TAcapacity, 500.0) * this.ORE_WEIGHT); // 1.0
         
         // dist evaluation [1000, 0]
-        int distEval = (int) (this.concaveDecreasingFunc(t.pathDist, 1000.0, 80.0, 300.0)*this.DIST_WEIGHT); // 1.0
+        int distEval = (int) (this.concaveDecreasingFunc(t.pathDist, 500.0, 40.0, 300.0)*this.DIST_WEIGHT); // 1.0
 
         // congestion eval [500, 0]
         double nearTaskT = this.timeSchedule.evaluateEventSlot(t.startTime, t.endTime, t.partner);
-        nearTaskT = nearTaskT == -1.0 ? 30.0 : nearTaskT < 30.0 ? nearTaskT : 30.0;
-        int congestionEval = (int) (this.linearIncreasingComparingFunc(nearTaskT, 0.0, 30.0, 1000.0) * this.CONGESTION_WEIGHT); // 0.5
+        nearTaskT = nearTaskT == -1.0 ? 60.0 : nearTaskT;
+        int congestionEval = (int) (this.linearIncreasingComparingFunc(nearTaskT, 0.0, 60.0, 500.0) * this.CONGESTION_WEIGHT); // 0.5
 
-        this.print("with robot-->"+m.sender +" dist-->"+ String.format("%.2f",t.pathDist) 
-            +" distanceEval-->"+distEval
-            +"\t nearTaskT-->"+String.format("%.2f",nearTaskT) 
-            +" congEnval-->"+congestionEval);
+        // this.print("with robot-->"+m.sender +" dist-->"+ String.format("%.2f",t.pathDist) 
+        //     +" distanceEval-->"+distEval
+        //     +"\t nearTaskT-->"+String.format("%.2f",nearTaskT) 
+        //     +" congEnval-->"+congestionEval
+        //     +",  total eval->"+(oreEval + distEval + congestionEval));
         
         return oreEval + distEval + congestionEval;
     }
